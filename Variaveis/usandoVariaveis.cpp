2c4b1ec505f3c274562ef9a7d4209024db3627d4
jonataspierre <68968625+jonataspierre@users.noreply.github.com>
2020-12-01 23:04:05 -0300

    Add files via upload

---
+++ Variaveis/usandoVariaveis.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include "tchar.h"
+//
+//int main()
+//{
+//	
+//	_tsetlocale(LC_ALL, _T("portuguese"));
+//	
+//	int NumVidas = 5;
+//	int Score = 1350;
+//	std::cout << "****** Início do Jogo **************" << std::endl;
+//	std::cout << "Vidas Jogador: " << NumVidas << std::endl;
+//	std::cout << "Pontuação: " << Score << std::endl;
+//	//std::cout << "Endereço de NumVidas na memória: " << &NumVidas << std::endl;
+//	std::cout << "Tamanho da variável NumVidas: " << sizeof(NumVidas) << std::endl;
+//	//std::cout << "Endereço de Pontuação na memória: " << &Score << std::endl;
+//	std::cout << "******************************************" << std::endl;
+//
+//	std::cout << "****** Durante o Jogo **************" << std::endl;
+//
+//	NumVidas = NumVidas - 1;
+//	Score = Score + 150;	
+//	std::cout << "Vidas Jogador: " << NumVidas << std::endl;
+//	std::cout << "Pontuação: " << Score << std::endl;
+//	std::cout << "******************************************" << std::endl;
+//
+//	system("pause");
+//
+//	return(0);
+//
+//}
